55ec1f0dada416085cdd0a06af3ea3ea20e854d0
Benjamin Liu <bliu94539@gmail.com>
2013-05-30 20:59:05 -0700

    blargh almost done, so close...

--- simpletimer/application.js
+++ simpletimer/application.js
@@ -1,3 +1,4 @@
+
 function updateTimer(){
     //Normal Timer
     if(countTarget == null){
@@ -10,10 +11,27 @@ function updateTimer(){
     if(currentSeconds == countTarget)
     {
       currentSeconds = countTarget;
-      document.getElementById("pause0").disabled = true;
-      document.getElementById("reset0").disabled = true;
+      $("#normal").find("pause").attr("disabled", "true");
+      $("#normal").find("reset").attr("disabled", "true");;
     } else if(currentSeconds > countTarget){
       currentSeconds = countTarget;
+    }
+
+    //Reverse Timer
+    var reverseTime = new Date();
+    if(!paused1){
+      reverseSeconds -= 1;
+    }
+    if(reverseSeconds == 0){
+      reverseSeconds = 0;
+      $("#reverse").find("pause").attr("disabled", "true");
+      $("#reverse").find("reset").attr("disabled", "true");;
+    } else if(reverseSeconds < 0){
+      reverseSeconds = 0;
+    }
+
+
+    displayTimer();
 }
 
 function displayTimer(){
@@ -31,73 +49,70 @@ function displayTimer(){
     var reverseTimerString = reverseDays + " days, " + reverseHours + " hours, " + reverseMinutes + " minutes, " + reverseSecondsDisplay + " seconds";
 
     // Update the time display
-    document.getElementById("clock0").firstChild.nodeValue = TimerString;
-    document.getElementById("clock1").firstChild.nodeValue = reverseTimerString;
+    $("clock0").text(TimerString);
+    $("clock1").text(reverseTimerString);
 }
 
-function pause(pausecase){
-    switch(pausecase){
-      case 0:
-      paused0 = !paused0;
-      if(paused0){
-        document.getElementById("pause0").value= "Start";
-      }
-      else{
-        document.getElementById("pause0").value= "Pause";
-      }
-      break
-      case 1:
-      paused1 = !paused1;
-      if(paused1){
-        document.getElementById("pause1").value= "Start";
-      }
-      else{
-        document.getElementById("pause1").value= "Pause";
-      }
-      break
-    }
-}
-
-function reset(resetcase){
-    var fdcurrentTime = new Date();
-    switch(resetcase){
-      case 0:
-      currentSeconds = 0;
-      break
-      case 1:
-      reverseSeconds = countTarget;
-      break
-      case 2:
-      if(Number(document.getElementById("input").value > 0)){
-        countTarget = Number(document.getElementById("input").value);
-      }
-      else{
-        alert("Please use a positive integer.");
-      }
-      currentSeconds = 0;
-      reverseSeconds = countTarget;
-    }
-}
+var countTarget;
+var currentSeconds = 0;
+var reverseSeconds = 0;
+var paused0 = false;
+var paused1 = false;
 
 $(document).ready(function() {
-    var countTarget;
-    var currentSeconds = 0;
-    var reverseSeconds = 0;
-    var paused0 = false;
-    var paused1 = false;
-
-    //Reverse Timer
-    var reverseTime = new Date();
-    if(!paused1){
-      reverseSeconds -= 1;
-    }
-    if(reverseSeconds == 0){
-      reverseSeconds = 0;
-      document.getElementById("pause1").disabled = true;
-      document.getElementById("reset1").disabled = true;
-    } else if(reverseSeconds < 0){
-      reverseSeconds = 0;
-    }
     displayTimer();
-    }
+    setInterval('updateTimer()', 1000);
+
+    //Clicking on Submit
+    $("body").on("click", "#submit", function(){
+        if($("input").val() > 0){
+            countTarget = $("input").val();
+        }
+        else{
+            alert("Please use a positive integer.");
+        }
+        currentSeconds = 0;
+        reverseSeconds = countTarget;
+    });
+
+    //Clicking on Reset
+    $("p").on("click", "#reset",
+        { str: $(this).closest("h2").text() },
+        function(event) {
+        var currentTime = new Date();
+        switch(event.data.str){
+          case "Normal Timer":
+          currentSeconds = 0;
+          break
+          case "Reverse Timer":
+          reverseSeconds = countTarget;
+          break
+        }
+    });
+
+    //Clicking on Pause
+    $("p").on("click", "#pause", 
+        { str: $(this).closest("h2").text() },
+        function(event) {
+        switch(pausecase){
+            case "Normal Timer":
+            paused0 = !paused0;
+            if(paused0){
+                $("#normal").("pause").attr("value", "Start");
+            }
+            else{
+               $("#normal").("pause").attr("value", "Pause");
+            }
+            break
+            case "Reverse Timer":
+            paused1 = !paused1;
+            if(paused1){
+                $("#reverse").("pause").attr("value", "Start");
+            }
+            else{
+                $("#reverse").("pause").attr("value", "Pause");
+            }
+            break
+        }
+    });
 });
